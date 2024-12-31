3c85330a3a3eb0c88b3189b51d36c73121f17403
Okechukwu88 <nichokechukwu@gmail.com>
2024-12-31 12:40:57 +0100

    commit new form changes

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'info@iapanel.ng';
+  $receiving_email_address = 'info@iapanel.abj.gov.ng';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'iapanel.abj.gov.ng',
+    'username' => 'info@iapanel.abj.gov.ng',
+    'password' => 'Sonicom@80',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
